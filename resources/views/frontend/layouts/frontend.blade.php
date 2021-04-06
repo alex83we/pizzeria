fdaee686bae11896e1253caac9c7e98b5881caea
aguth <aguthmann83@gmail.com>
2021-04-06 22:53:05 +0200

    Fixed

--- resources/views/frontend/layouts/frontend.blade.php
+++ resources/views/frontend/layouts/frontend.blade.php
@@ -41,25 +41,6 @@ Burger, Pizza, Döner, Kurdische Gerichte, Salat, Getränke')">
     <meta name="twitter:image" content="https://www.buttstaedter-bistro.de/images/logo.jpg">
     <link rel="canonical" href="{{ url()->full() }}"/>
 
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicon/apple-icon-57x57.png') }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicon/apple-icon-60x60.png') }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicon/apple-icon-72x72.png') }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/apple-icon-76x76.png') }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicon/apple-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicon/apple-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicon/apple-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicon/apple-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicon/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('favicon/manifest.json') }}">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="{{ asset('favicon/ms-icon-144x144.png') }}">
-    <meta name="theme-color" content="#ffffff">
-
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -85,7 +66,7 @@ Burger, Pizza, Döner, Kurdische Gerichte, Salat, Getränke')">
 </footer>
 
 <!-- Back to Top -->
-<div id="back-to-top"><a class="top arrow"><i class="fa fa-chevron-up"></i></a></div>
+<div id="back-to-top"><button class="btn top arrow" style="line-height: 0;"><i class="fa fa-chevron-up"></i></button></div>
 
 <!-- Optional JavaScript -->
 <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
