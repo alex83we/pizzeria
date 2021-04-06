68690c47e1b04b8195790b1a59b459a6ad5b99d4
aguth <aguthmann83@gmail.com>
2021-04-06 09:44:56 +0200

    Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Category;
+use App\Models\Admin\Zutaten;
+use App\Models\Frontend\Speisekarte;
 
 class DashboardController extends Controller
 {
 
     public function index()
     {
-        return view('admin.dashboard');
+        $speisekarte = Speisekarte::count();
+        $kategorien = Category::count();
+        $zutaten = Zutaten::count();
+        return view('admin.dashboard', compact('kategorien', 'speisekarte', 'zutaten'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,40 @@
 @endpush
 
 @section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="fas fa-bars"></i> </span>
 
+                    <div class="info-box-content">
+                        <span class="info-box-text">Kategorien gesamt</span>
+                        <span class="info-box-number">{{ $kategorien }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-secondary"><i class="fas fa-pizza-slice"></i> </span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Zutaten gesamt</span>
+                        <span class="info-box-number">{{ $zutaten }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-success"><i class="fas fa-file"></i> </span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Gericht gesamt</span>
+                        <span class="info-box-number">{{ $speisekarte }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 
 @push('js')
